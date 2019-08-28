91d6986a85ed52e4e31fd655269331015949f961
st133 <s_t133@student.usc.edu.au>
2019-08-28 23:00:52 +1000

    arguments attached to fragment

--- app/src/main/java/android/bignerdranch/com/CrimeActivity.java
+++ app/src/main/java/android/bignerdranch/com/CrimeActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
-    public static final String EXTRA_CRIME_ID =
+    private static final String EXTRA_CRIME_ID =
             "android.bignerdranch.com.criminalintent.crime_id";
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
@@ -20,6 +20,8 @@ public class CrimeActivity extends SingleFragmentActivity {
     }
     @Override
     protected Fragment createFragment(){
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
